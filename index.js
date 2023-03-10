ea96469cd1ec8a8b1e4239a936bd68e18254b088
Shawon Khondoker <shawonkhondoker0.com>
2023-03-10 15:40:43 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// var Score = 7;
+
+// if (Score >=90 && Score <= 100) {
+//     console.log('A+')
+// }
+// else if (Score>=80, Score <= 100) {
+//     console.log('byy')
+
+//     if( Score <= 89, Score>=70){
+//         console.log('A-')
+//     }
+//     else if(Score>=89, Score <= 70){
+//         console.log('byyy');
+//     }
+//     if( Score <= 79, Score>=60){
+//         console.log('c')
+//     }
+//     else if(Score>=89, Score <= 70){
+//         console.log('byyy4');
+//     }
+//     if( Score <= 69, Score>=40){
+//         console.log('D')
+//     }
+//     else if(Score>=89, Score <= 70){
+//         console.log('byyy5');
+//     }
+//     if(Score>=0, Score <= 39){
+//         console.log('F')
+//     }
+//     else if(Score>=39, Score >= 1){
+//         console.log('byyy6');
+//     }
+// }
+
+// for loop
+
+
+// chang .........'let' 
+// nun-chang.......'const'
+for (let i = 0; i <=100; i++) {
+    console.log(i) ; 
+}
+for (let i = 0; i <=100; i+=2) {
+    console.log(i) ; 
+}
+for (let i = 0; i <=100; i+=3) {
+    console.log(i) ; 
+}
