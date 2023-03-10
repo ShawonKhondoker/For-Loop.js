95a0186d8e7b901ab6cb6b51d85b2de1c9be9598
Shawon Khondoker <shawonkhondoker0.com>
2023-03-10 15:43:44 -0700

    2nt commit

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-// var Score = 7;
+var Score = 7;
 
-// if (Score >=90 && Score <= 100) {
-//     console.log('A+')
-// }
-// else if (Score>=80, Score <= 100) {
-//     console.log('byy')
+if (Score >=90 && Score <= 100) {
+    console.log('A+')
+}
+else if (Score>=80, Score <= 100) {
+    console.log('byy')
 
-//     if( Score <= 89, Score>=70){
-//         console.log('A-')
-//     }
-//     else if(Score>=89, Score <= 70){
-//         console.log('byyy');
-//     }
-//     if( Score <= 79, Score>=60){
-//         console.log('c')
-//     }
-//     else if(Score>=89, Score <= 70){
-//         console.log('byyy4');
-//     }
-//     if( Score <= 69, Score>=40){
-//         console.log('D')
-//     }
-//     else if(Score>=89, Score <= 70){
-//         console.log('byyy5');
-//     }
-//     if(Score>=0, Score <= 39){
-//         console.log('F')
-//     }
-//     else if(Score>=39, Score >= 1){
-//         console.log('byyy6');
-//     }
-// }
+    if( Score <= 89, Score>=70){
+        console.log('A-')
+    }
+    else if(Score>=89, Score <= 70){
+        console.log('byyy');
+    }
+    if( Score <= 79, Score>=60){
+        console.log('c')
+    }
+    else if(Score>=89, Score <= 70){
+        console.log('byyy4');
+    }
+    if( Score <= 69, Score>=40){
+        console.log('D')
+    }
+    else if(Score>=89, Score <= 70){
+        console.log('byyy5');
+    }
+    if(Score>=0, Score <= 39){
+        console.log('F')
+    }
+    else if(Score>=39, Score >= 1){
+        console.log('byyy6');
+    }
+}
 
 // for loop
 
